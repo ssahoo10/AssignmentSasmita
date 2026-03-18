7e333ea5bd7c204ccc1f29adc3d0a4effa60b6b8
ssahoo10 <sahoosasmita.in@gmail.com>
2026-03-18 17:16:54 +0530

    Add files via upload
    updated the comment

--- assignment/week1/day2/scoping.js
+++ assignment/week1/day2/scoping.js
@@ -6,10 +6,10 @@ let  color = "brown"
 if(genderType==="female"){
     var age = 30
     let color = "pink"
-console.log("Coluor inside the block is " +color);
+console.log("Colour inside the if block is " +color);
 }
-console.log("Age of the person is " +age);
-console.log("Coluor outside the block is " +color);
+console.log("Age of the person outside if-block is " +age);
+console.log("Colour outside the if-block is " +color);
 }
 printGender()
 console.log("gender is " +genderType);
