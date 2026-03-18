1e608f02c45eecd93128b866027503d7b57e3147
ssahoo10 <sahoosasmita.in@gmail.com>
2026-03-18 16:27:13 +0530

    Data Type Assignment for Day1

---
+++ dataTypeDay1.js
@@ -0,0 +1,32 @@
+
+//Premitive Data Types
+
+//String
+let userName="Sasmita"
+console.log(userName);
+console.log(typeof userName);
+
+let organization = "TestLeaf"
+console.log(organization);
+console.log(typeof organization);
+
+// Number
+let contactNumber = 9535519752
+console.log(contactNumber);
+console.log(typeof contactNumber);
+
+// Boolean 
+let knowsAutomation = false
+console.log(knowsAutomation);
+console.log(typeof knowsAutomation);
+
+ 
+// Undefined (not assigned) 
+let usesPlaywright
+console.log(usesPlaywright);
+console.log(typeof usesPlaywright);
+
+
+
+
+
