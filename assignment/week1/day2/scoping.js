bf3425f5495381290f07a13d193b0734de35bcfc
ssahoo10 <sahoosasmita.in@gmail.com>
2026-03-18 17:09:56 +0530

    Add files via upload
    Uploaded the scoping assignment.

---
+++ assignment/week1/day2/scoping.js
@@ -0,0 +1,17 @@
+
+
+let genderType = "female"
+function printGender(){
+let  color = "brown"
+if(genderType==="female"){
+    var age = 30
+    let color = "pink"
+console.log("Coluor inside the block is " +color);
+}
+console.log("Age of the person is " +age);
+console.log("Coluor outside the block is " +color);
+}
+printGender()
+console.log("gender is " +genderType);
+genderType = "male"
+console.log("gender is " +genderType);
