95ee8fdbc0aa7f5842040dfa17e547ab1290ffde
ssahoo10 <sahoosasmita.in@gmail.com>
2026-03-18 16:41:26 +0530

    Add files via upload
    Assignment for Week1-Day1 and Day2 is uploaded.

---
+++ assignment/week1/day2/numberType.js
@@ -0,0 +1,22 @@
+const { log } = require("node:console");
+
+function checkNumberType(){
+let num=-1
+
+if(num>0){
+    console.log("Positive number")
+}
+else if(num<0){
+console.log("Negative number")
+} 
+else {
+    console.log("Neutral");  
+}
+}
+checkNumberType()
+
+//let myNum
+//console.log(checkNumberType(myNum=10))
+//console.log(checkNumberType(myNum=-10))
+//console.log(checkNumberType(myNum=0))
+
---
+++ assignment/week1/day2/oddEven.js
@@ -0,0 +1,17 @@
+/* Assignment Requirements:  
+1. Create a function named `isOddOrEven` that takes a number as a parameter   
+2. Declare and initialize the variable   
+3.  Use a conditional statement to check if the number is divisible by 2  
+4. Call the function and print the result 
+*/
+
+function isOddOrEven(){
+const num=12
+if(num%2===0){
+    console.log(num+ " is an Even number")
+}
+else{
+console.log(num+ " is an Odd number")
+}
+}
+isOddOrEven()
